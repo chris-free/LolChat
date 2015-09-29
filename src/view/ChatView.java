d08a3f98c7fee2d504ad44beb4f0f67790bc412b
Chris Freeman <chris.fy@gmail.com>
2015-09-29 17:44:38 +0100

    list observer for presence change

--- src/view/ChatView.java
+++ src/view/ChatView.java
@@ -1,7 +1,6 @@
 package view;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
@@ -21,6 +20,10 @@ import javafx.util.Callback;
 import model.Api;
 import model.Summoner;
 
+import com.github.theholywaffle.lolchatapi.LolStatus;
+import com.github.theholywaffle.lolchatapi.LolStatus.GameStatus;
+import com.github.theholywaffle.lolchatapi.LolStatus.Queue;
+
 public class ChatView {
 
 	private ListWrapper listWrapper;
@@ -34,15 +37,33 @@ public class ChatView {
 	}
 
 	static class ColorRectCell extends ListCell<Summoner> {
+		
+		private Text textName = new Text();
+		
+		private Text textStatus = new Text();
+		
+		public void updatePresence(Summoner summoner) {
+			LolStatus lolStatus = summoner.getStatus();
+			GameStatus queue = lolStatus.getGameStatus();
+			if(summoner.isOnline() && queue != null) {
+				System.out.println(summoner.getName() + queue.toString() + " " + queue.internal());
+				if (queue == GameStatus.IN_GAME) {
+				} 
+				textName.setText(summoner.getName() + " - " + queue.internal());
+			}
+		}
+		
 		@Override
-		public void updateItem(Summoner item, boolean empty) {
-			super.updateItem(item, empty);
-			if (item != null) {
+		public void updateItem(Summoner summoner, boolean empty) {
+			super.updateItem(summoner, empty);
+			if (summoner != null) {
+				System.out.println("list updateItem" + summoner.getName());
+				summoner.registerPresenceObserver(() -> updatePresence(summoner));
 				HBox box = new HBox();
-				Text t = new Text(item.getFriend().getName());
-				item.getFriend().getName();
-				box.getChildren().add(t);
-				setGraphic(t);
+				textName.setText(summoner.getName());
+				box.getChildren().add(textName);
+				box.getChildren().add(textStatus);
+				setGraphic(textName);
 			}
 		}
 	}
