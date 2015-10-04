fb20b39f0ff90ba630338ed7012973339f0a36b1
Chris Freeman <chris.fy@gmail.com>
2015-10-04 18:29:39 +0100

    Cell presence listeningf

--- src/view/SummonerCell.java
+++ src/view/SummonerCell.java
@@ -2,6 +2,8 @@ package view;
 
 import javafx.scene.control.ListCell;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 import model.Summoner;
 
@@ -9,33 +11,50 @@ import com.github.theholywaffle.lolchatapi.LolStatus;
 import com.github.theholywaffle.lolchatapi.LolStatus.GameStatus;
 
 class SummonerCell extends ListCell<Summoner> {
-	
+
 	private Text textName = new Text();
-	
-	private Text textStatus = new Text();
-	
+
+	private Circle statusCircle = new Circle(0, 0, 5, Color.GREY);
+
 	public void updatePresence(Summoner summoner) {
-		LolStatus lolStatus = summoner.getStatus();
-		GameStatus queue = lolStatus.getGameStatus();
-		if(summoner.isOnline() && queue != null) {
-			System.out.println(summoner.getName() + queue.toString() + " " + queue.internal());
-			if (queue == GameStatus.IN_GAME) {
-			} 
-			textName.setText(summoner.getName() + " - " + queue.internal());
+		if(summoner.isOnline()) {
+			switch (summoner.getChatMode()) {
+			case AVAILABLE:
+				statusCircle.setFill(Color.GREEN);
+				break;
+			case AWAY:
+				statusCircle.setFill(Color.YELLOW);
+				break;
+			case BUSY: 
+				statusCircle.setFill(Color.RED);
+				break;
+			default:
+				statusCircle.setFill(Color.GRAY);
+			}
+
+			LolStatus lolStatus = summoner.getStatus();
+			GameStatus queue = lolStatus.getGameStatus();
+			if(queue != null) {
+				System.out.println(summoner.getName() + queue.toString() + " " + queue.internal());
+				textName.setText(summoner.getName() + " - " + queue.internal());
+			}
+		} else {
+			statusCircle.setFill(Color.GRAY);
 		}
 	}
-	
+
 	@Override
 	public void updateItem(Summoner summoner, boolean empty) {
 		super.updateItem(summoner, empty);
 		if (summoner != null) {
 			System.out.println("list updateItem" + summoner.getName());
-			summoner.registerPresenceObserver(() -> updatePresence(summoner));
+			//summoner.registerPresenceObserver(() -> updatePresence(summoner));
 			HBox box = new HBox();
 			textName.setText(summoner.getName());
+		//	box.getChildren().add(statusCircle);
 			box.getChildren().add(textName);
-			box.getChildren().add(textStatus);
-			setGraphic(textName);
+			setGraphic(box);
+			updatePresence(summoner);
 		}
 	}
 }
