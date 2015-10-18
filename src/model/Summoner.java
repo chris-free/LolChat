de715500084085560fb9caff363573fcd330f6f8
Chris Freeman <chris.fy@gmail.com>
2015-10-18 17:54:15 +0100

    fixing concurrent modification exception

--- src/model/Summoner.java
+++ src/model/Summoner.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
 
 import com.github.theholywaffle.lolchatapi.ChatMode;
 import com.github.theholywaffle.lolchatapi.LolStatus;
@@ -13,17 +14,17 @@ public class Summoner {
 
 	protected Friend friend;
 	private List <Runnable> presenceConsumers = Collections.synchronizedList(new ArrayList<Runnable>());
-	
+
 	public Summoner(Friend friend) {
 		this.friend = friend;
 	}
-	
+
 	public void notifyPresenceObservers() {
-		for (Runnable statusObserver : presenceConsumers) {
-			statusObserver.run();
-		}
+		presenceConsumers.stream()
+						 .collect(Collectors.toList())
+						 .forEach(Runnable::run);
 	}
-	
+
 	public void registerPresenceObserver(Runnable o) {
 		presenceConsumers.add(o);
 	}
@@ -31,38 +32,38 @@ public class Summoner {
 	public void registerChatObserver(BiConsumer<Summoner, String> listener) {
 		friend.setChatListener((Friend friend, String message) ->  listener.accept(this, message));
 	}
-	
+
 	public void removePresenceObserver(Runnable o) {
 		presenceConsumers.remove(o);
 	}
-	
+
 	public void sendMessage(String message) {
 		friend.sendMessage(message);
 		System.out.println("sending " + message);
 	}
-	
+
 	public Friend getFriend() {
 		return friend;
 	}
-	
+
 	public String getUserId() {
 		return friend.getUserId();
 	}
-	
+
 	public String getName() {
 		return friend.getName();
 	}
-	
+
 	public boolean isOnline() {
 		return friend.isOnline();
 	}
-	
+
 	public LolStatus getStatus() {
 		return friend.getStatus();
 	}
-	
+
 	public ChatMode getChatMode() {
 		return friend.getChatMode();
 	}
-	
+
 }
